a28146240d0a9239fd2fbf4ac4db9eb3a7e4f06c
Hujaifa81 <abuhojaifa123@gmail.com>
2023-10-14 13:23:12 +0600

    task-3

---
+++ lab-task-4-phpFormValidation/registration.php
@@ -0,0 +1,16 @@
+
+<?php
+$name = $REQUEST["name"];
+$email = $REQUEST["email"];
+$userName = $REQUEST["userName"];
+$password = $REQUEST["password"];
+$confirmPassword = $REQUEST["confirmPassword"];
+$gender = $REQUEST["gender"];
+$day = $REQUEST["day"];
+$month = $REQUEST["month"];
+$year = $REQUEST["year"];
+
+if ($name === null || $email === null || $userName === null || $password === null || $confirmPassword === null) {
+    echo "All fields are required.";
+}
+?>
