17a3f2f6996d2a26f2ac3455304f710df5665922
Hujaifa81 <abuhojaifa123@gmail.com>
2023-10-23 16:16:52 +0600

    done

--- lab-task-5-phpLayout/account.php
+++ lab-task-5-phpLayout/account.php
@@ -1,9 +1,9 @@
 <h3>Account<h3>
 <hr>
 <ul>
-<p><a href="dashboard.php">Dashboard</a></p>
-<p><a href="viewProfile.php">View profile</a></p>
-<p><a href="editProfile.php">Edit Profile</a></p>
-<p><a href="changeProfilePicture.php">Change profile picture</a></p>
-<p><a href="logout.php">Logout</a></p>
+<li><a href="dashboard.php">Dashboard</a></li>
+<li><a href="viewProfile.php">View profile</a></li>
+<li><a href="editProfile.php">Edit Profile</a></li>
+<li><a href="changeProfilePicture.php">Change profile picture</a></li>
+<li><a href="logout.php">Logout</a></li>
 </ul>
